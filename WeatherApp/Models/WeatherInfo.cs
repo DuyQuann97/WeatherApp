6979bc13d7b35bdf223e4f5e94e6c1be47b6c3c9
DQuan97 <ngduyquan97@gmail.com>
2023-08-19 21:36:34 +0700

    them Json WeatherInfor

---
+++ WeatherApp/Models/WeatherInfo.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Models
+{
+    public class WeatherInfo
+    {
+        public Location Location { get; set; }
+    }
+
+    public class  Location 
+    {
+        public string Name { get; set; }
+
+        [JsonProperty("lat")]
+        public double Latitude { get; set; }
+        [JsonProperty("lon")]
+        public double Longtitude{ get; set; } 
+    }
+}
--- WeatherApp/ViewModels/MainViewModel.cs
+++ WeatherApp/ViewModels/MainViewModel.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO.Packaging;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using System.Windows.Navigation;
+using WeatherApp.Models;
 
 namespace WeatherApp.ViewModels
 {
@@ -33,10 +36,27 @@ namespace WeatherApp.ViewModels
 
         public MainViewModel()
         {
-            SearchCommand = new RelayCommand<object>(_ => { return true; }, _ =>
+            SearchCommand = new RelayCommand<object>(_ => { return true; }, async _ =>
             {
                 // Handling event/command
-                string searchedCity = City;
+                var client = new HttpClient();
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri("https://weatherapi-com.p.rapidapi.com/current.json?q=Ho%20Chi%20Minh"),
+                    Headers =
+                    {
+                        { "X-RapidAPI-Key", "1a19c73a31mshf423af852816df1p1f63a4jsnfb24cc4d42e6" },
+                        { "X-RapidAPI-Host", "weatherapi-com.p.rapidapi.com" },
+                    },
+                };
+
+                var response = await client.SendAsync(request);
+                var body = await response.Content.ReadAsStringAsync();
+                
+                // Map convert data Json
+
+                var Infor = JsonConvert.DeserializeObject<WeatherInfo>(body);
             });
         }
     }
