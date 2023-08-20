c68a350f3076c5cc054baecc1856a3ae930d8265
DQuan97 <ngduyquan97@gmail.com>
2023-08-20 16:51:30 +0700

    Add Models : Temperature , Condition and UV

--- WeatherApp/Models/WeatherInfo.cs
+++ WeatherApp/Models/WeatherInfo.cs
@@ -11,15 +11,41 @@ namespace WeatherApp.Models
     public class WeatherInfo
     {
         public Location Location { get; set; }
+        public Current Current { get; set; }
+
+        
     }
 
     public class  Location 
     {
+        // Name City 
         public string Name { get; set; }
 
+        // Latitude & Longtitude
         [JsonProperty("lat")]
         public double Latitude { get; set; }
         [JsonProperty("lon")]
-        public double Longtitude{ get; set; } 
+        public double Longtitude{ get; set; }
+
+    }
+
+    public class Current
+    {
+        [JsonProperty("temp_c")]
+        public float TempC { get; set; }
+        [JsonProperty("temp_f")]
+        public float TempF { get; set; }
+        public float uv {get; set; }
+        public Condition condition { get; set; }
+
+
+        public class Condition
+        {
+     
+            public string text { get; set; }
+            
+            public string icon { get; set; }
+        }
+
     }
 }
